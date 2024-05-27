0219a1f84806531139691ce8fbcaa96424647f87
Michael River Nilsson <mrn@michaelriver.dev>
2024-05-27 19:38:02 +0200

    Updates temporary home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,37 @@
 import Link from "next/link";
+import Image from "next/image";
+import { SiNextdotjs } from "react-icons/si";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-5 pt-24 bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-      <div>
-        <h1 className="text-2xl text-zinc-200">
-          New portfolio for <span className=" text-zinc-500">Michael River Nilsson</span> is coming soon.
+    <main className="flex min-h-screen flex-col gap-5 items-center mx-auto py-12 ">
+      <div className="mt-24">
+        <div className="w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+        <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl whitespace-nowrap bg-clip-text text-center py-5">
+          Michael River Nilsson
         </h1>
-        <p>
-          Meanwhile, feel free to check out the{" "}
-          <Link href="https://www.github.com/rivermichael" target="_blank" className="text-zinc-500">
-            GitHub page
+        <div className="w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      </div>
+      <div className="flex flex-col items-center justify-between grow mb-24">
+        <div className="text-zinc-200 animate-fade-in">
+          <h1 className="text-2xl">New portfolio is coming soon.</h1>
+          <p>
+            Meanwhile, feel free to check out the{" "}
+            <Link href="https://www.github.com/rivermichael" target="_blank" className="text-zinc-500">
+              GitHub page
+            </Link>
+            .
+          </p>
+        </div>
+
+        <div className="flex gap-10 items-center animate-fade-in">
+          <Link href="https://www.vercel.com" target="_blank">
+            <Image src="/vercel.svg" alt="Vercel Logo" className="dark:invert" width={100} height={24} />
           </Link>
-          .
-        </p>
+          <Link href="https://www.nextjs.org" target="_blank">
+            <Image src="/next.svg" alt="NextJS Logo" className="dark:invert" width={100} height={24} />
+          </Link>
+        </div>
       </div>
     </main>
   );
