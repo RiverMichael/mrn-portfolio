b6b58a1ad8705add0c9a15f1aa9e0ea35ac671ba
Michael River Nilsson <mrn@michaelriver.dev>
2024-05-29 13:57:29 +0200

    Adds copyright text in footer

--- app/components/ui/Footer/index.jsx
+++ app/components/ui/Footer/index.jsx
@@ -1,3 +1,14 @@
+import Link from "next/link";
+
 export default function Footer() {
-  return <footer className="mt-auto"></footer>;
+  return (
+    <footer className="mt-auto p-5">
+      <p className="text-center text-sm">
+        Copyright &copy; 2024 |{" "}
+        <Link href="mailto:mrn@michaelriver.dev" className="link">
+          Michael River Nilsson
+        </Link>
+      </p>
+    </footer>
+  );
 }
