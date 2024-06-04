a10ab7fc7068cbc32069f677ab4b324664438e73
Michael River Nilsson <mrn@michaelriver.dev>
2024-06-04 13:10:16 +0200

    Changes alt texts on Skills section to optimize accessibility

--- app/components/SkillsSection/index.jsx
+++ app/components/SkillsSection/index.jsx
@@ -17,8 +17,8 @@ export default function SkillsSection() {
           target="_blank"
         >
           <div className="skill-item">
-            <Image src="/html.svg" alt="HTML" width={40} height={40} />
-            <he className="text-sm">HTML</he>
+            <Image src="/html.svg" alt="HTML icon" width={40} height={40} />
+            <span className="text-sm">HTML</span>
           </div>
         </Link>
 
@@ -27,8 +27,8 @@ export default function SkillsSection() {
           target="_blank"
         >
           <div className="skill-item">
-            <Image src="/css.svg" alt="CSS" width={40} height={40} />
-            <h3 className="text-sm">CSS</h3>
+            <Image src="/css.svg" alt="CSS icon" width={40} height={40} />
+            <span className="text-sm">CSS</span>
           </div>
         </Link>
 
@@ -39,18 +39,18 @@ export default function SkillsSection() {
           <div className="skill-item">
             <Image
               src="/javascript.svg"
-              alt="Javascript"
+              alt="Javascript icon"
               width={40}
               height={40}
             />
-            <h3 className="text-sm">JavaScript</h3>
+            <span className="text-sm">JavaScript</span>
           </div>
         </Link>
 
         <Link href="https://react.dev" target="_blank">
           <div className="skill-item">
-            <Image src="/reactjs.svg" alt="React" width={40} height={40} />
-            <h3 className="text-sm">React</h3>
+            <Image src="/reactjs.svg" alt="React icon" width={40} height={40} />
+            <span className="text-sm">React</span>
           </div>
         </Link>
 
@@ -58,19 +58,19 @@ export default function SkillsSection() {
           <div className="skill-item">
             <Image
               src="/nextjs.svg"
-              alt="Next js"
+              alt="Next js icon"
               width={40}
               height={40}
               className="invert"
             />
-            <h3 className="text-sm">Next.js</h3>
+            <span className="text-sm">Next.js</span>
           </div>
         </Link>
 
         <Link href="https://vitejs.dev" target="_blank">
           <div className="skill-item">
-            <Image src="/vite.svg" alt="Vite" width={40} height={40} />
-            <h3 className="text-sm">Vite</h3>
+            <Image src="/vite.svg" alt="Vite icon" width={40} height={40} />
+            <span className="text-sm">Vite</span>
           </div>
         </Link>
 
@@ -78,11 +78,11 @@ export default function SkillsSection() {
           <div className="skill-item">
             <Image
               src="/tailwind.svg"
-              alt="Tailwind CSS"
+              alt="Tailwind icon"
               width={40}
               height={40}
             />
-            <h3 className="text-sm">Tailwind</h3>
+            <span className="text-sm">Tailwind</span>
           </div>
         </Link>
 
@@ -90,67 +90,82 @@ export default function SkillsSection() {
           <div className="skill-item">
             <Image
               src="/bootstrap.svg"
-              alt="Bootstrap"
+              alt="Bootstrap icon"
               width={40}
               height={40}
             />
-            <h3 className="text-sm">Bootstrap</h3>
+            <span className="text-sm">Bootstrap</span>
           </div>
         </Link>
 
         <Link href="https://sass-lang.com/" target="_blank">
           <div className="skill-item">
-            <Image src="/sass.svg" alt="Sass" width={40} height={40} />
-            <h3 className="text-sm">Sass</h3>
+            <Image src="/sass.svg" alt="Sass icon" width={40} height={40} />
+            <span className="text-sm">Sass</span>
           </div>
         </Link>
 
         <Link href="https://daisyui.com" target="_blank">
           <div className="skill-item">
-            <Image src="/daisyui.svg" alt="DaisyUi" width={40} height={40} />
-            <h3 className="text-sm">DaisyUi</h3>
+            <Image
+              src="/daisyui.svg"
+              alt="DaisyUi icon"
+              width={40}
+              height={40}
+            />
+            <span className="text-sm">DaisyUi</span>
           </div>
         </Link>
 
         <Link href="https://flowbite.com" target="_blank">
           <div className="skill-item">
-            <Image src="/flowbite.svg" alt="Flowbite" width={40} height={40} />
-            <h3 className="text-sm">Flowbite</h3>
+            <Image
+              src="/flowbite.svg"
+              alt="Flowbite icon"
+              width={40}
+              height={40}
+            />
+            <span className="text-sm">Flowbite</span>
           </div>
         </Link>
 
         <Link href="https://vitest.dev" target="_blank">
           <div className="skill-item">
-            <Image src="/vitest.svg" alt="Vitest" width={40} height={40} />
-            <h3 className="text-sm">Vitest</h3>
+            <Image src="/vitest.svg" alt="Vitest icon" width={40} height={40} />
+            <span className="text-sm">Vitest</span>
           </div>
         </Link>
 
         <Link href="https://cypress.io" target="_blank">
           <div className="skill-item">
-            <Image src="/cypress.svg" alt="Cypress" width={40} height={40} />
-            <h3 className="text-sm">Cypress</h3>
+            <Image
+              src="/cypress.svg"
+              alt="Cypress icon"
+              width={40}
+              height={40}
+            />
+            <span className="text-sm">Cypress</span>
           </div>
         </Link>
 
         <Link href="https://jestjs.io" target="_blank">
           <div className="skill-item">
-            <Image src="/jest.svg" alt="Jest" width={40} height={40} />
-            <h3 className="text-sm">Jest</h3>
+            <Image src="/jest.svg" alt="Jest icon" width={40} height={40} />
+            <span className="text-sm">Jest</span>
           </div>
         </Link>
 
         <Link href="https://git-scm.com" target="_blank">
           <div className="skill-item">
-            <Image src="/git.svg" alt="Git" width={40} height={40} />
-            <h3 className="text-sm">Git</h3>
+            <Image src="/git.svg" alt="Git icon" width={40} height={40} />
+            <span className="text-sm">Git</span>
           </div>
         </Link>
 
         <Link href="https://github.com" target="_blank">
           <div className="skill-item">
-            <Image src="/github.svg" alt="GitHub" width={40} height={40} />
-            <h3 className="text-sm">GitHub</h3>
+            <Image src="/github.svg" alt="GitHub icon" width={40} height={40} />
+            <span className="text-sm">GitHub</span>
           </div>
         </Link>
 
@@ -158,18 +173,18 @@ export default function SkillsSection() {
           <div className="skill-item">
             <Image
               src="/wordpress.svg"
-              alt="WordPress"
+              alt="WordPress icon"
               width={40}
               height={40}
             />
-            <h3 className="text-sm">WordPress</h3>
+            <span className="text-sm">WordPress</span>
           </div>
         </Link>
 
         <Link href="https://figma.com" target="_blank">
           <div className="skill-item">
-            <Image src="/figma.svg" alt="Figma" width={40} height={40} />
-            <h3 className="text-sm">Figma</h3>
+            <Image src="/figma.svg" alt="Figma icon" width={40} height={40} />
+            <span className="text-sm">Figma</span>
           </div>
         </Link>
       </div>
